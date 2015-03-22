40dad2b25c2f94cd4138a807df4b5d94b4605aee
Marcel van der Boom <marcel@hsdev.com>
2015-03-22 17:07:22 +0100

    Simplify the org convert

--- _plugins/org_converter.rb
+++ _plugins/org_converter.rb
@@ -1,7 +1,6 @@
 require 'org-ruby'
 
-
-# Augment String with a to_b method : converts sensibly to boolean
+# Augment String class with a to_b method : converts sensibly to boolean
 class String
   def to_b
     return true if self =~ (/^(true|t|yes|y|1)$/i)
@@ -17,7 +16,7 @@ module Jekyll
     safe true
 
     def matches(ext)
-      ext =~ /[.]org/i
+      ext =~ /[.]org$/i
     end
 
     def output_ext(ext)
@@ -25,21 +24,15 @@ module Jekyll
     end
 
     def convert(content)
-      content
+      org_text = Orgmode::Parser.new(content, {markup_file: "html.tags.yml" })
+      org_text.in_buffer_settings.delete("TITLE")  # We already captured the title  
+      org_text.to_html
     end
 
-    def self.quote_replace(content)
-      content = content.gsub("&#8216;","'") #&lsquo;
-      content = content.gsub("&#8217;","'") #&rsquo;
-      content = content.gsub("&#8220;",'"') #&ldquo;
-      content = content.gsub("&#8221;",'"') #&rdquo;
-      content = content.gsub("&#39;"  ,"'") #&apos;
-    end
-
-    def self.process_options(org_text, data)
+    def self.process_options(content, data)
+      org_text = Orgmode::Parser.new(content, {markup_file: "html.tags.yml" })
+      
       org_text.in_buffer_settings.each_pair do |key, value|
-        # Remove #+TITLE from the buffer settings to avoid double rendering
-        org_text.in_buffer_settings.delete(key) if key =~ /title/i
         # We need true/false as booleans, not string. 
         if key.downcase == 'published' #(any others?)
           value = value.to_b
@@ -70,65 +63,29 @@ module Jekyll
     end
   end
   
-  # Handle included org files
-  module Tags
-    class IncludeTag
-      alias :_orig_read_file :read_file
-      
-      def read_file(file, context)
-        if file !~ /[.]org$/
-          return _orig_read_file(file, context)
-        end
-
-        # Read contents of the included file        
-        content = File.read(file, file_read_opts(context))
-
-        # Parse it
-        org_text = Orgmode::Parser.new(content, { markup_file: "html.tags.yml" })
-
-        # FIXME: Do we want/need to process the buffer settings?
-        # Convert the orgmode format via org-ruby to html
-        OrgConverter::quote_replace(org_text.to_html)
-      end
-    end
-  end
-
   # Handle Convertibles: Posts and Pages
   module Convertible
     # Override the read_yaml method to take org buffer settings instead
     alias :_orig_read_yaml :read_yaml
 
     def read_yaml(base, name, opts = {})
-      # We only process org files, call parent for others
+      # We only process org files, call parent for other
       if name !~ /[.]org$/
         return _orig_read_yaml(base, name)
       end
       
       # Read in file and set defaults
-      content = File.read(File.join(base, name), merged_file_read_opts(opts))
+      self.content = File.read(File.join(base, name), merged_file_read_opts(opts))
       self.data ||= {}
 
       # Read in all buffer settings in orgmode syntax and set them as value
-      org_text = Orgmode::Parser.new(content, { markup_file: "html.tags.yml" })
-      self.data = OrgConverter::process_options(org_text, self.data)
-    
-      # Convert the orgmode format via org-ruby to html
-      self.content = OrgConverter::quote_replace(org_text.to_html)
-
-      # Make sure post excerpts are pointing to the right content
-      # FIXME: this doesn't seem to work properly
-      if self.type == "posts"
-        puts self.extract_excerpt
-        self.extracted_excerpt = self.extract_excerpt
-      end
-       
+      self.data = OrgConverter::process_options(content, self.data)
     rescue => e
       puts "Error converting file #{File.join(base, name)}: #{e.message} #{e.backtrace}"
     end
   end
 
   # Document is somehow not a Convertible, so we have to do the whole thing again.
-  # The class is a bit different though
   class Document
     alias :_orig_read :read
     
@@ -143,13 +100,8 @@ module Jekyll
         unless defaults.empty?
           @data = defaults
         end
-        content = File.read(path, merged_file_read_opts(opts))
-        
-        org_text = Orgmode::Parser.new(content, { markup_file: "html.tags.yml" })
-        @data = OrgConverter::process_options(org_text, @data)
-        
-        # Convert the orgmode format via org-ruby to html
-        self.content = OrgConverter::quote_replace(org_text.to_html)
+        self.content = File.read(path, merged_file_read_opts(opts))
+        @data = OrgConverter::process_options(content, @data)
       end
         
     rescue SyntaxError => e
