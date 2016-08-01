1bda26b43e842ffcca2a7ccbf5e450af3a341e32
Marcel van der Boom <marcel@hsdev.com>
2016-08-01 12:53:22 +0200

    Snapshot commit with several things working 'well enough'
    - get multiple versions (source and html) from one source format working
    - get the tag pages working
    - get some functions in place
    - rearrange a couple of things
    - basic prev/next functionality (too much, is on all post renderings
    - now)

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,15 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
 
 -- System imports
 import           Data.Monoid   ((<>))
-import           Data.List (groupBy)
+import           Data.List (groupBy, intercalate, sortBy)
 import           Data.Function (on)
-import           System.FilePath (takeBaseName)
+import           System.FilePath (takeBaseName, takeFileName)
 
 -- Hakyll imports
 import           Hakyll
@@ -14,13 +17,23 @@ import           Hakyll
 -- Our own imports
 import Config
 import Compiler.Org
-import Template.Functions
+import Template.Functions (json, striptags)
 
+-- Stuff for the testing area
+import Control.Applicative (Alternative (..))
+import Data.Time.Format (parseTimeM, defaultTimeLocale)
+-- older TimeLocale import?
+--import System.Locale (defaultTimeLocale)
+import Data.Time.Clock (UTCTime)
+import Control.Monad (liftM, filterM)
+
+-- Patterns which match blog postings
 postsPattern :: Pattern
 postsPattern =
   "sites/main/_posts/2*.org" 
 --  .&&. complement "sites/main/_posts/_*.org"
 
+-- A few ad-hoc orgmode documents which should be published as pages
 orgPages :: Pattern
 orgPages =
   "about/*.org"
@@ -32,16 +45,11 @@ main :: IO ()
 main = hakyllWith config $ do
     templateR  -- Make sure that our templates are compiled
     staticR    -- Copy static files
-
     aboutR     -- About pages
-    
     postR      -- Process posts
     feedR      -- Process atom feed
     homeR      -- Homepage
-
-    tags <- buildTags postsPattern (fromCapture "tag/*/index.html")
-    tagsR tags -- List of posts tagged with certain tags
-
+    tagsR      -- List of posts tagged with certain tags
     archiveR   -- Grouped list of all posts
 
     match "sitemap.xml"    plainApply  -- Generate sitemap.xml
@@ -56,12 +64,14 @@ plainApply :: Rules ()
 plainApply = do
   route idRoute
   compile $ do
-    posts <- recentFirst =<< loadAllSnapshots (postsPattern .&&. hasVersion "html") "content"
+    posts <- recentFirst =<< loadAllSnapshots (postsPattern .&&. hasVersion "html") "html"
+    
     let ctx =
           listField "posts" postCtx (return posts) <>
           baseContext
     getResourceBody >>= applyAsTemplate ctx
-          
+
+-- Search results (javascript based)
 searchR :: Rules ()
 searchR =
   match "search/index.html" $ do
@@ -73,17 +83,23 @@ searchR =
         >>= loadAndApplyTemplate "_layouts/page.html" baseContext
         >>= loadAndApplyTemplate "_layouts/default.html" baseContext
         >>= relativizeUrls
-        
-    
+
 -- Grouped view of tag filter post
-tagsR :: Tags -> Rules ()
-tagsR tags = 
+-- FIXME: this generates contentless pages, why?
+-- 
+tagsR :: Rules ()
+tagsR = do
+  tags <- buildTags postsPattern (fromCapture "tag/*/index.html")
+
+  -- Use the tagsRules' function to generate the pages
+  -- FIXME: I don't think I understand 'pattern'  here, I could only get it to work
+  --        by filtering on the tag.
   tagsRules tags $ \tag pattern -> do
     let title = "Posts tagged \"" ++ tag ++ "\""
       
     route idRoute
-    compile $ groupedPostList title pattern
-    
+    compile $ groupedPostList title (tagFilter tag) postsPattern
+
 -- Archives are the same as the tag page, just more postings and
 -- a different title
 archiveR :: Rules ()
@@ -91,28 +107,11 @@ archiveR =
   create ["archive/index.html"] $ do
     let title = "Archives"
     route idRoute
-    
-    compile $ groupedPostList title postsPattern
 
--- Common part of tag/<tag>/index.html pages and archive/index.html page
-groupedPostList :: String -> Pattern -> Compiler (Item String)
-groupedPostList title pattern = do
-  posts <- fmap groupByYear $
-    recentFirst
-    =<< loadAllSnapshots (pattern .&&. hasVersion "html") "content"
-  let ctx =
-        constField "title" title <>
-        listField "years"
-        (
-          field "year" (return . fst . itemBody) <>
-          listFieldWith "posts" postCtx (return . snd . itemBody)
-        )
-        (sequence $ fmap (\(y, is) -> makeItem (show y, is)) posts) <>
-        baseContext
-  makeItem ""
-    >>= loadAndApplyTemplate "_layouts/grouped.html" ctx
-    >>= loadAndApplyTemplate "_layouts/default.html" ctx
-    >>= relativizeUrls
+    -- recentFirst is already in groupedPostList
+    -- HOWTO: combine filter here?
+    compile $ groupedPostList title recentFirst postsPattern
+
 
 -- Homepage rules
 homeR :: Rules ()
@@ -122,7 +121,7 @@ homeR =
 
    compile $ do
      posts <- fmap (take 5) . recentFirst
-             =<< loadAllSnapshots (postsPattern .&&. hasVersion "html") "content"
+             =<< loadAllSnapshots (postsPattern .&&. hasVersion "html") "html"
      let indexCtx =
            listField  "posts"      postCtx (return posts) <>
            constField "title"      "Home" <>
@@ -169,8 +168,14 @@ aboutR = do
   -- Generate resolved source format
   match orgPages $ version "source" $ do
     route idRoute
-    compile $ getResourceString
-        >>= applyAsTemplate baseContext
+
+    compile $ do
+      let ctx = 
+            constField "testing" "testing" <>
+            --constField "site.posts.size" (getpostCount postsPattern) <>
+            baseContext
+      getResourceString
+        >>= applyAsTemplate ctx
         >>= saveSnapshot "source"
 
   -- Generate html rendering based on source
@@ -183,12 +188,38 @@ aboutR = do
         >>= loadAndApplyTemplate "_layouts/default.html" baseContext
         >>= relativizeUrls
 
+
+--groupedPostList :: String -> ([Item a] -> Compiler [Item String]) -> Pattern -> Compiler (Item String)
+groupedPostList title customFilter pattern = do
+  posts <- fmap groupByYear $
+    recentFirst
+    =<< customFilter
+    =<< loadAllSnapshots (pattern .&&. hasVersion "html") "html"
+  let ctx =
+        constField "title" title <>
+        listField "years"
+        (
+          field "year" (return . fst . itemBody) <>
+          listFieldWith "posts" postCtx (return . snd . itemBody)
+        )
+        (sequence $ fmap (\(y, is) -> makeItem (show y, is)) posts) <>
+        baseContext
+  makeItem ""
+    >>= loadAndApplyTemplate "_layouts/grouped.html" ctx
+    >>= loadAndApplyTemplate "_layouts/default.html" ctx
+    >>= relativizeUrls
+
+-- Shorthand to pass filters to grouped list
+tagFilter :: MonadMetadata m => String -> [Item a] -> m [Item a]
+tagFilter tag = filterM (fmap (elem tag) . getTags . itemIdentifier)
+
+
 -- Get a previously compiled item from a version
 itemFromVersion :: String -> Compiler (Item String)
 itemFromVersion v = do
   id' <- setVersion (Just v) `fmap` getUnderlying
-  -- FIXME: this is not a version, it's a snapshot name
-  body <- loadSnapshotBody id' v 
+  -- Q: can we use a snapshot here? 
+  body <- loadBody id'  
   makeItem body
   
 -- Post Rules
@@ -201,19 +232,22 @@ itemFromVersion v = do
 --   Previous/Next links?
 postR :: Rules ()
 postR = do
+  -- Resolve variables and save as 'source' version
   match postsPattern $ version "source" $ do
     route $ postsRoute "org"
     compile $ getResourceString
       >>= applyAsTemplate postCtx
       >>= saveSnapshot "source"
-      
+
+  -- Generate 'html' version based on 'source' version
   match postsPattern $ version "html" $ do
     route $ postsRoute "html"
 
-    -- Compile posts with the orgmode compiler
+    -- Compile posts with the orgmode compiler, this generates the
+    -- metadata from the header too
     compile $ orgCompilerWith (itemFromVersion "source")
       >>= loadAndApplyTemplate "_layouts/post.html"    postCtx
-      >>= saveSnapshot "content"
+      >>= saveSnapshot "html"
       >>= loadAndApplyTemplate "_layouts/default.html" postCtx
       >>= relativizeUrls
 
@@ -242,11 +276,10 @@ postCtx =
 
     listFieldWith "taglist" tagCtx getTagItems <>
 
-    constField "excerpt" "" <>
+    -- Our templates use reverse terminolog
+    field "prevPost" nextPostUrl <>
+    field "nextPost" previousPostUrl <>
 
-    -- Expose somet functions to the templates
-    functionField "json"      json      <>
-    functionField "striptags" striptags <>
     baseContext
   where
       -- Construct the 'tag' inside the list field (do we need url here too?)
@@ -268,7 +301,7 @@ feedR =
     route idRoute
     compile $ do
       posts <- fmap (take 20) . recentFirst
-              =<< loadAllSnapshots (postsPattern .&&. hasVersion "html") "content"
+              =<< loadAllSnapshots (postsPattern .&&. hasVersion "html") "html"
       renderAtom feedConfiguration feedCtx posts
    where
      feedCtx :: Context String
@@ -284,16 +317,21 @@ baseContext =
   constField "site.author" author <>
   constField "year"        copyrightYear <>
 
+  -- Expose some functions to the templates
+  functionField "json"      json      <>
+  functionField "striptags" striptags <>
+
+
   -- $body$, $url$, $path$ and all $foo$ from metadata are delivered
   -- by the default context
   defaultContext
   
--- Group by year, depending on filename, so FIXME later
+-- Group by year, depending on filename, so FIXME later to use
+-- metadata for this function
 groupByYear :: [Item a] -> [(Int, [Item a])]
-groupByYear = map (\pg -> ( year (head pg), pg) ) .
-              groupBy ((==) `on` year)
-   where year :: Item a -> Int
-         year = read . take 4 . takeBaseName . toFilePath . itemIdentifier
+groupByYear = map (\pg -> ( year (head pg), pg) ) .  groupBy ((==) `on` year)
+  where year :: Item a -> Int
+        year = read . take 4 . takeBaseName . toFilePath . itemIdentifier
 
 -- For later inspection
 -- This give the latest commit for a file
@@ -301,3 +339,64 @@ commitField :: Context String
 commitField = field "commit" $ \item -> do 
   let fp = toFilePath $ itemIdentifier item 
   unixFilter "git" ["log", "-1", "--oneline", fp] "" 
+
+
+--- TESTING TESTING ----
+-- Get the number of posts that satisfy a pattern
+postCount :: MonadMetadata m => Pattern -> m Int
+postCount p =  length <$> getMatches p
+
+-- getpostCount :: Pattern -> String 
+-- getpostCount p = (postCount p)
+  
+  
+previousPostUrl :: Item String -> Compiler String
+previousPostUrl post = do
+    posts <- getMatches (postsPattern .&&. hasVersion "html")
+    let ident = itemIdentifier post
+        sortedPosts = sortIdentifiersByDate posts
+        ident' = itemBefore sortedPosts ident
+    case ident' of
+        Just i -> (fmap (maybe empty toUrl) . getRoute) i
+        Nothing -> empty
+
+
+nextPostUrl :: Item String -> Compiler String
+nextPostUrl post = do
+    posts <- getMatches (postsPattern .&&. hasVersion "html")
+    let ident = itemIdentifier post
+        sortedPosts = sortIdentifiersByDate posts
+        ident' = itemAfter sortedPosts ident
+    case ident' of
+        Just i -> (fmap (maybe empty toUrl) . getRoute) i
+        Nothing -> empty
+
+
+itemAfter :: Eq a => [a] -> a -> Maybe a
+itemAfter xs x =
+    lookup x $ zip xs (tail xs)
+
+
+itemBefore :: Eq a => [a] -> a -> Maybe a
+itemBefore xs x =
+    lookup x $ zip (tail xs) xs
+
+
+sortIdentifiersByDate :: [Identifier] -> [Identifier]
+sortIdentifiersByDate =
+  sortBy (flip byDate)
+  where
+    byDate id1 id2 =
+      let fn1 = takeFileName $ toFilePath id1
+          fn2 = takeFileName $ toFilePath id2
+          parseTime' fn = parseTimeM True defaultTimeLocale "%Y-%m-%d" $ intercalate "-" $ take 3 $ splitAll "-" fn
+      in compare (parseTime' fn1 :: Maybe UTCTime) (parseTime' fn2 :: Maybe UTCTime)
+ 
+
+-- Possible helpers for draft functionlity by inspecting a 'draf't metadata field
+isNotDraft :: MonadMetadata m => Identifier -> m Bool
+isNotDraft identifier = liftM (/= Just "true") (getMetadataField identifier "draft")
+
+filterDrafts :: MonadMetadata m => [Identifier] -> m [Identifier]
+filterDrafts  = filterM isNotDraft 
+
